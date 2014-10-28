8e2f4683bf779b35df8ee8846c64fdaea9d6e705
asimbhatti786 <asim_aali@live.com>
2014-10-28 16:11:10 +0500

    first commit

---
+++ app/controllers/visitor_controller.rb
@@ -0,0 +1,6 @@
+class VisitorController < ApplicationController
+  def new
+    @owner = Owner.new
+  end
+  
+end
---
+++ test/helpers/visitors_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class VisitorsControllerHelperTest < ActionView::TestCase
+end
