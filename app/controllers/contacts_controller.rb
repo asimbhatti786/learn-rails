a1b632d837e9090b6fd18fbc14540f65d385b465
asimbhatti786 <asim_aali@live.com>
2014-10-29 16:11:55 +0500

    third commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,59 +1,45 @@
 class ContactsController < ApplicationController
-  
   #def process_form
   #Rails.logger.debug "DEBUG: params are #{params}"
   #flash[:notice] = "Received request from #{params}[:contact][:name]"
   
   #redirect_to root_path
   def new
-@contact = Contact.new
-end
-
-def create
-
-  @contact = Contact.new(secure_params)
-
-  if @contact.valid?
-
-
-    #@contact.update_spreadsheet
-
-    UserMailer.contact_email(@contact).deliver
-
-    flash[:notice] = "Message sent from #{@contact.name}."
-
-    redirect_to root_path
-
-  else
-
-    render :new
-
+    @contact = Contact.new
   end
-end
 
-private
-
-def secure_params
-
-
-  params.require(:contact).permit(:name, :email)
-end
-  def new                    #method is created named 'new'
-    @contact = Contact.new     #create a instance variable
-  end
-  
   def create
     
     @contact = Contact.new(secure_params)
+
     if @contact.valid?
-      # TODO save data
-      # TODO send message
+
+
+      #@contact.update_spreadsheet
+
+      UserMailer.contact_email(@contact).deliver
+
       flash[:notice] = "Message sent from #{@contact.name}."
+
       redirect_to root_path
+
     else
+
       render :new
+
     end
   end
+
+  private
+
+  def secure_params
+
+
+    params.require(:contact).permit(:name, :email)
+  end
+  
+  
+  
   private
   def secure_params
     params.require(:contact).permit(:name, :email, :content)
